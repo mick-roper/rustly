2c7caf22c1472e064663965e40d8b4df0c802108
mick roper <mick@theropers.co.uk>
2022-05-04 11:47:02 +0100

    added map generation
    Signed-off-by: mick roper <mick@theropers.co.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
 use specs::prelude::*;
-use std::cmp::{max, min};
 use specs_derive::Component;
+use std::cmp::{max, min};
 
 #[derive(Component)]
 struct State {
     ecs: World
 }
 
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
+        // 1: handle input
+        player_input(self, ctx);
+
+        // 2: update state
+        self.run_systems();
+
+        // 3: render
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
     }
 }
@@ -34,6 +49,131 @@ struct Renderable {
     bg: RGB,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Component)]
+struct LeftMover {}
+
+struct LeftWalker {}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -=1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50().with_title("Rustly").build()?;
+
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
+
+    gs.ecs.create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK)
+        })
+        .with(Player{})
+        .build();
+
+    for i in 0..10 {
+        gs.ecs.create_entity()
+            .with(Position { x: i*7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('x'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK)
+            })
+            .with(LeftMover{})
+            .build();
+    }
+
+    rltk::main_loop(context, gs)
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        }
+    }
+}
+
+fn xy_idx(x: i32, y: i32) -> usize {
+    (y * 80 + x) as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let w:i32 = 80;
+    let h:i32 = 50;
+
+    let mut map = vec![TileType::Floor; (w*h) as usize];
+
+    for x in 0..w {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, w-1)] = TileType::Wall;
+    }
+
+    for y in 0..h {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(h-1, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _ in 0..400 {
+        let x = rng.roll_dice(1, w-1);
+        let y = rng.roll_dice(1, h-1);
+        let idx = xy_idx(x, y);
+        let exclude = xy_idx(40, 25);
+        if idx != exclude {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
 }
