e17010a6ff8c2a916a90af24290171a4ff97634d
mick roper <mick@theropers.co.uk>
2022-05-04 08:50:59 +0100

    started adding types
    Signed-off-by: mick roper <mick@theropers.co.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+#[derive(Component)]
+struct State {
+    ecs: World
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            
+        }
+    }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
 fn main() {
     println!("Hello, world!");
 }
