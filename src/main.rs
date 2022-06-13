e2df473849ad0b8c159acc34f9d84b610574a3fd
mick roper <mick@theropers.co.uk>
2022-06-13 15:32:53 +0100

    more refactoring
    Signed-off-by: mick roper <mick@theropers.co.uk>

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
+        let map = self.ecs.fetch::<Map>();
         draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
@@ -51,9 +51,9 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map = Map::new();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
+    let (player_x, player_y) = map.rooms[0].center();
 
     gs.ecs
         .create_entity()
