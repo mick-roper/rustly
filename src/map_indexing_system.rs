ad1e4f3288b0ebde82cc42f9cea47f5299d319dc
mick roper <mick@theropers.co.uk>
2022-07-26 20:09:32 +0100

    fmtted the code
    Signed-off-by: mick roper <mick@theropers.co.uk>

--- src/map_indexing_system.rs
+++ src/map_indexing_system.rs
@@ -1,20 +1,22 @@
+use super::{BlocksTile, Map, Position};
 use specs::prelude::*;
-use super::{Map, Position, BlocksTile};
 
 pub struct MapIndexingSystem {}
 
 impl<'a> System<'a> for MapIndexingSystem {
-    type SystemData = ( WriteExpect<'a, Map>,
-                        ReadStorage<'a, Position>,
-                        ReadStorage<'a, BlocksTile>);
+  type SystemData = (
+    WriteExpect<'a, Map>,
+    ReadStorage<'a, Position>,
+    ReadStorage<'a, BlocksTile>,
+  );
 
-    fn run(&mut self, data : Self::SystemData) {
-        let (mut map, position, blockers) = data;
+  fn run(&mut self, data: Self::SystemData) {
+    let (mut map, position, blockers) = data;
 
-        map.populate_blocked();
-        for (position, _blocks) in (&position, &blockers).join() {
-            let idx = map.xy_idx(position.x, position.y);
-            map.blocked[idx] = true;
-        }
+    map.populate_blocked();
+    for (position, _blocks) in (&position, &blockers).join() {
+      let idx = map.xy_idx(position.x, position.y);
+      map.blocked[idx] = true;
     }
+  }
 }
