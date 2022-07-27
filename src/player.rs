efc49b216e0755e9f47d0f9704927ad54f534247
mick roper <mick@theropers.co.uk>
2022-07-27 09:09:24 +0100

    added some simple code to hit a target
    Signed-off-by: mick roper <mick@theropers.co.uk>

--- src/player.rs
+++ src/player.rs
@@ -1,17 +1,30 @@
-use rltk::{VirtualKeyCode, Rltk, Point};
+use rltk::{VirtualKeyCode, Rltk, Point, console};
 use specs::prelude::*;
-use super::{Position, Player, Map, TileType, State, Viewshed, RunState};
+use super::{Position, Player, Map, State, Viewshed, RunState, CombatStats};
 use std::cmp::{min, max};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
     let mut viewsheds = ecs.write_storage::<Viewshed>();
-    let map = ecs.fetch::<Map>();
     let mut player_pos = ecs.write_resource::<Point>();
+    let map = ecs.fetch::<Map>();
+    let combat_stats = ecs.read_storage::<CombatStats>();
 
     for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        for potential_target in map.tile_content[destination_idx].iter() {
+            let target = combat_stats.get(*potential_target);
+            match target {
+                None => {}
+                Some(_t) => {
+                    console::log("From hells heart I stab at thee!");
+                    return;
+                }
+            }
+        }
+
         if !map.blocked[destination_idx] {
             pos.x = min(79 , max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
